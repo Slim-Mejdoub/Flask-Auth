0fb72a0d186837e7cc859d694fdd2f9da5cbc7b6
slim <slimmoj@gmail.com>
2022-11-12 13:48:35 +0900

    Second Commit

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user, login_manager
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.environ.get("APP_CONFIG")
-print(app.config['SECRET_KEY'])
+app.config['SECRET_KEY'] = os.getenv("app.config['SECRET_KEY']")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-app.secret_key = os.environ.get("APP_SECRET_KEY")
-print(app.secret_key)
-
+app.secret_key = os.getenv("app.secret_key")
 
 login_manager = LoginManager()
 login_manager.init_app(app)
